4f163ec36e9f88c2207c9a412c64cd4e4e0111d6
Canh Kieu <minhcanhdn0704@gmail.com>
2020-04-26 13:55:07 -0700

    Upload hình ảnh

--- pages/brands/add.vue
+++ pages/brands/add.vue
@@ -1,49 +1,78 @@
 <template>
-  <v-card>
-    <v-form @submit.prevent="save">
-      <v-img
-        class="align-end justify-center"
-        :aspect-ratio="16 / 9"
-        :src="brand.cover"
-      >
-        <v-row align="center" justify="center">
-          <v-avatar size="150" class="mb-5">
-            <v-btn color="pink" dark small absolute bottom fab>
+  <v-row align="center" justify="center">
+    <v-col cols="6" lg="5">
+      <v-card>
+        <v-form @submit.prevent="save">
+          <v-img
+            class="align-end justify-center"
+            :aspect-ratio="16 / 9"
+            :src="coverPath"
+          >
+            <v-row align="center" justify="center">
+              <v-avatar size="150" class="mb-5">
+                <v-btn
+                  color="pink"
+                  dark
+                  small
+                  absolute
+                  bottom
+                  fab
+                  @click="$refs.logoInput.click()"
+                >
+                  <v-icon>mdi-camera</v-icon>
+                </v-btn>
+                <img :src="logoPath" alt="" />
+              </v-avatar>
+            </v-row>
+            <v-btn
+              color="pink"
+              @click="$refs.coverInput.click()"
+              dark
+              small
+              absolute
+              top
+              right
+              fab
+              style="top: 20px;"
+            >
               <v-icon>mdi-camera</v-icon>
             </v-btn>
-            <img src="https://cdn.vuetifyjs.com/images/john.jpg" alt="John" />
-          </v-avatar>
-        </v-row>
-        <v-btn
-          color="pink"
-          dark
-          small
-          absolute
-          top
-          right
-          fab
-          style="top: 20px;"
-        >
-          <v-icon>mdi-camera</v-icon>
-        </v-btn>
-      </v-img>
+          </v-img>
 
-      <v-card-text>
-        <v-text-field label="Tên salon" type="text" v-model="brand.name">
-        </v-text-field>
-        <v-text-field label="Mô tả" type="text" v-model="brand.description">
-        </v-text-field>
-        <v-text-field label="Địa chỉ" type="text" v-model="brand.address">
-        </v-text-field>
-      </v-card-text>
-      <v-card-actions>
-        <v-spacer></v-spacer>
-        <v-btn color="primary" :loading="$apollo.loading" type="submit"
-          >Lưu</v-btn
-        >
-      </v-card-actions>
-    </v-form>
-  </v-card>
+          <v-card-text>
+            <input
+              type="file"
+              ref="logoInput"
+              accept="image/*"
+              @change="logoPicked($event, 'logo')"
+            />
+            <input
+              type="file"
+              ref="coverInput"
+              accept="image/*"
+              @change="logoPicked($event, 'cover')"
+            />
+            <v-text-field label="Logo" type="text" v-model="brand.logo">
+            </v-text-field>
+            <v-text-field label="Cover" type="text" v-model="brand.cover">
+            </v-text-field>
+            <v-text-field label="Tên salon" type="text" v-model="brand.name">
+            </v-text-field>
+            <v-text-field label="Mô tả" type="text" v-model="brand.description">
+            </v-text-field>
+            <v-text-field label="Địa chỉ" type="text" v-model="brand.address">
+            </v-text-field>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="primary" :loading="$apollo.loading" type="submit"
+              >Lưu</v-btn
+            >
+          </v-card-actions>
+        </v-form>
+      </v-card>
+    </v-col>
+  </v-row>
 </template>
 
 <script>
@@ -76,8 +105,45 @@ export default {
   },
   data: () => ({
     brand: {},
+    uploadFileStatus: false,
   }),
+  computed: {
+    logoPath() {
+      return process.env.API_URL + this.brand.logo
+    },
+    coverPath() {
+      return process.env.API_URL + this.brand.cover
+    },
+  },
   methods: {
+    logoPicked(e, type) {
+      const files = e.target.files
+      if (files[0] !== undefined) {
+        this.uploadFileStatus = true
+        this.$apollo
+
+          .mutate({
+            mutation: gql`
+              mutation UploadLogo($type: String!, $image: Upload!) {
+                upload(image: $image, type: $type)
+              }
+            `,
+            variables: {
+              type: type,
+              image: files[0],
+            },
+          })
+          .then(({ data }) => {
+            this.uploadFileStatus = false
+            this.$set(this.brand, type, data.upload)
+
+          })
+          .catch((error) => {
+            this.uploadFileStatus = false
+            console.error(error)
+          })
+      }
+    },
     save() {
       this.$apollo
         .mutate({
